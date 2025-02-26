bf9f5d8e66e2411ab43ae113099be0bb7af86baa
sohel <mr.sohelsyed@gmail.com>
2025-02-26 18:32:09 +0530

    update api and url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("ws://real-time-news-feed-be.onrender.com/");
+const socket = io("ws://real-time-news-feed-be.onrender.com/", {
+  transports: ["websocket"],
+  withCredentials: true,
+});
 
 export default socket;
