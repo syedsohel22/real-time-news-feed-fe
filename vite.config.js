7c83a2089bdf7efc15d2a582dc0ab9c47d46db49
sohel <mr.sohelsyed@gmail.com>
2025-02-26 18:46:50 +0530

    changed the vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    host: "0.0.0.0", // Allows access from external networks
+    port: 5173,
+  },
+
+  preview: {
+    host: "0.0.0.0",
+    port: 4173, // Adjust if needed
+    allowedHosts: ["real-time-news-feed-fe.onrender.com"], // Add your frontend Render domain
+  },
 });
